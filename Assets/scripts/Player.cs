65def8b3b0735163b6dd56f781285cd9d9bbb5e4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-21 22:11:23 +0000

    Import zoort zombie model and set up animations

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Animator anim;
+
+	private void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	private void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Jump ();
+		}
+	}
+
+	private void Jump () {
+		anim.Play ("Jump");
+	}
+}
