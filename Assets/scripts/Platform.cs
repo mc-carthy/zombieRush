ecb8b046d173e19a05e33305fe6b0e3b3fd01ab9
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-21 19:15:20 +0000

    Platforms move and are replaced once off screen

---
+++ Assets/scripts/Platform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	[SerializeField]
+	private float platformSpeed = 2;
+	private float resetPosX = -30f;
+	private float spawnPosX = 31f;
+
+	private void Update () {
+		transform.Translate(Vector3.left * platformSpeed * Time.deltaTime);
+
+		if (transform.localPosition.x <= resetPosX) {
+			Vector3 newPos = new Vector3 (
+				spawnPosX,
+				transform.position.y,
+				transform.position.z
+			);
+			transform.position = newPos;
+		}
+	}
+}
